586c16c864851f8ced5a3f4224eead3304ac8498
NatalyaBah <104036796+NatalyaBah@users.noreply.github.com>
2022-08-16 12:07:13 +0300

    Add files via upload

---
+++ ДЗ_1.cpp
@@ -0,0 +1,54 @@
+﻿// ДЗ_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+using namespace std;
+#include <iostream>
+
+//**********************Задание 1*****************************
+
+int main()
+{
+    cout << "Gruppa:\tQueen\n";
+    cout << "Pesnya:\tThe Show Must Go On\n";
+    cout << "Slova:\tThe show must go on\n";
+    cout << "\tThe show must go on, yeah\n";
+    cout << "\tInside my heart is breaking\n";
+    cout << "\tMy makeup may be flaking\n";
+    cout << "\tBut my smile, still, stays on\n";
+
+    //******************Задание 2*****************************
+
+    cout << "\n";
+    cout << "Every\n";
+    cout << "\thunter\n";
+    cout << "\t\twants\n";
+    cout << "\t\t\tto\n";
+    cout << "\t\t\t\tknow\n";
+    cout << "\t\t\t\t\twhere\n";
+    cout << "\t\t\t\t\t\tthe\n";
+    cout << "\t\t\t\t\t\t\tFaesbeshniks\n";
+    cout << "\t\t\t\t\t\t\t\t\tare.\n";
+
+ 
+    //******************Задание 3*****************************
+
+    setlocale(LC_ALL, "rus");
+    cout << "\n";
+    cout << "\tСрочно!!! Продаю диван. Очень мягкий, красивый и удобный\n";
+    cout << "\tЗвонить по телефону 233-34-56, до 23.00\n";
+    cout << "\t" << char(151) << char(151) << char(151) << char(151) << char(151) << char(151) << char(151);
+    cout << char(151) << char(151) << char(151) << char(151) << char(151) << char(151) << char(151) << char (151);
+    cout << char(151) << char(151) << char(151) << char(151) << char(151) << char(151) << char(151) << char (151);
+    cout << char(151) << char(151) << char(151) << char(151) << char(151) << char(151) << char(151) << char(151);
+    cout << char(151) << char(151) << char(151) << char(151) << char(151) << char(151) << char(151) << char(151);
+    cout << char(151) << char(151) << char(151) << char(151) << char(151) << char(151) << char(151) << char(151);
+    cout << char(151) << char(151) << char(151) << char(151) << char(151) << char(151) << char(151) << char(151);
+    cout << char(151) << endl;
+    cout << "\t 233\t233\t233\t233\t233\t233\t233\n";
+    cout << "\n";
+    cout << "\t  34\t 34\t 34\t 34\t 34\t 34\t 34\n";
+    cout << "\n";
+    cout << "\t  56\t 56\t 56\t 56\t 56\t 56\t 56\n";
+
+}
+
