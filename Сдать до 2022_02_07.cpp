9ae04f11e16f7f129dc6c71140595697cb99e737
NatalyaBah <104036796+NatalyaBah@users.noreply.github.com>
2022-08-16 12:23:46 +0300

    Add files via upload

---
+++ Сдать до 2022_02_07.cpp
@@ -0,0 +1,286 @@
+﻿// Сдать до 2022_02_07.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+//Написать перегруженные функции (int, double, char) для выполнения 
+//следующих задач:
+
+//Инициализация квадратной матрицы типа int
+void initArray(int array[5][5], int a, int b);
+//Вывод матрицы на экран типа int
+void showArray(int array[5][5], int a, int b);
+//Определение максимального элемента на главной диагонали
+// матрицы типа int
+int maxElement(int array[5][5], int a, int b);
+//Определение минимального элемента на главной диагонали
+// матрицы типа int
+int minElement(int array[5][5], int a, int b);
+//Cортировка элементов по возрастанию отдельно для 
+// каждой строки матрицы типа int
+void sortArray(int array[5][5], int a, int b);
+//Инициализация квадратной матрицы типа double
+void initArray(double array[5][5], double a, double b);
+//Вывод матрицы на экран типа double
+void showArray(double array[5][5], double a, double b);
+//Определение максимального элемента на главной диагонали
+// матрицы типа double
+double maxElement(double array[5][5], int a, int b);
+//Определение минимального элемента на главной диагонали
+//матрицы типа double
+double minElement(double array[5][5], int a, int b);
+//Cортировка элементов по возрастанию отдельно для 
+//каждой строки матрицы типа double
+void sortArray(double array[5][5], int a, int b);
+//Инициализация квадратной матрицы типа char
+void initArray(char array[5][5], char a, char z);
+//Вывод матрицы на экран типа char
+void showArray(char array[5][5], char a, char b);
+//Определение максимального элемента на главной диагонали
+//матрецы типа char
+char maxElement(char array[5][5], int a, int b);
+//Определение минимального элемента на главной диагонали
+//матрицы типа char
+char minElement(char array[5][5], int a, int b);
+//Сортировка жлементов по возрастанию отдельно для 
+//каждой строки матрицы типа char
+void sortArray(char array[5][5], int a, int b);
+
+
+void initArray(int array[5][5], int a, int b)
+{
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            array[i][j] = 10 + rand() % (90 - 10) + 1;
+        }
+    }
+}
+void showArray(int array[5][5], int a, int b)
+{
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0 ; j < b; j++)
+        {
+            cout << array[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+int maxElement(int array[5][5], int a, int b)
+{
+    int max = 0;
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            if (i == j)
+            {
+                if (array[i][j] > max)  max = array[i][j];                
+            }
+        }
+    }
+    return max;
+}
+int minElement(int array[5][5], int a, int b)
+{
+    int min = 90;
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            if (i == j)
+            {
+                if (array[i][j] < min)  min = array[i][j];
+            }
+        }
+    }
+    return min;
+}
+void sortArray(int array[5][5], int a, int b)
+{
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            for (int k = 0; k < b - 1; k++)
+            {
+                if (array[i][k] > array[i][k + 1]) swap(array[i][k], array[i][k + 1]);
+            }
+        }
+    }
+}
+void initArray(double array[5][5], int a, int b)
+{
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            array[i][j] = (1000 + rand() % (9000 - 1000 + 1)) / 100.0 + 0.01;
+        }
+    }
+}
+void showArray(double array[5][5], int a, int b)
+{
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            cout << array[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+double maxElement(double array[5][5], int a, int b)
+{
+    double max = 0;
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            if (i == j)
+            {
+                if (array[i][j] > max)  max = array[i][j];
+            }
+        }
+    }
+    return max;
+}
+double minElement(double array[5][5], int a, int b)
+{
+    double min = 90;
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            if (i == j)
+            {
+                if (array[i][j] < min)  min = array[i][j];
+            }
+        }
+    }
+    return min;
+}
+void sortArray(double array[5][5], int a, int b)
+{
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            for (int k = 0; k < b - 1; k++)
+            {
+                if (array[i][k] > array[i][k + 1]) swap(array[i][k], array[i][k + 1]);
+            }
+        }
+    }
+}
+void initArray(char array[5][5], int a, int z)
+{
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < z; j++)
+        {
+            array[i][j] = 97 + rand() % (122 - 97 + 1);
+        }
+    }
+}
+void showArray(char array[5][5], int a, int z)
+{
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < z; j++)
+        {
+            cout << array[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+char maxElement(char array[5][5], int a, int b)
+{
+    char max = 97;
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            if (i == j)
+            {
+                if (array[i][j] > max)  max = array[i][j];
+            }
+        }
+    }
+    return max;
+}
+char minElement(char array[5][5], int a, int b)
+{
+    char min = 122;
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            if (i == j)
+            {
+                if (array[i][j] < min)  min = array[i][j];
+            }
+        }
+    }
+    return min;
+}
+void sortArray(char array[5][5], int a, int b)
+{
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            for (int k = 0; k < b - 1; k++)
+            {
+                if (array[i][k] > array[i][k + 1]) swap(array[i][k], array[i][k + 1]);
+            }
+        }
+    }
+}
+
+
+
+int main()
+{
+    srand(time(NULL));
+    int array1[5][5];
+    double array2[5][5];
+    char array3[5][5];
+    cout << "Overloaded function int " << endl << endl;
+    initArray(array1, 5, 5);
+    showArray(array1, 5, 5);
+    cout << endl;
+    cout << "Max Elemetn: " << maxElement(array1, 5, 5) << endl;
+    cout << "Min Elemetn: " << minElement(array1, 5, 5) << endl;   
+    cout << endl;
+    cout << "Sorting an array line by line " << endl << endl;
+    sortArray(array1, 5, 5);
+    showArray(array1, 5, 5);
+    cout << endl;
+    cout << "Overloaded function double " << endl << endl;
+    initArray(array2, 5, 5);
+    showArray(array2, 5, 5);
+    cout << endl;
+    cout << "Max Elemetn: " << maxElement(array2, 5, 5) << endl;
+    cout << "Min Elemetn: " << minElement(array2, 5, 5) << endl;
+    cout << endl;
+    cout << "Sorting an array line by line " << endl << endl;
+    sortArray(array2, 5, 5);
+    showArray(array2, 5, 5);
+    cout << endl;
+    cout << "Overloaded function char " << endl << endl;
+    initArray(array3, 5, 5);
+    showArray(array3, 5, 5);
+    cout << endl;
+    cout << "Max Elemetn: " << maxElement(array3, 5, 5) << endl;
+    cout << "Min Elemetn: " << minElement(array3, 5, 5) << endl;
+    cout << endl;
+    cout << "Sorting an array line by line " << endl << endl;
+    sortArray(array3, 5, 5);
+    showArray(array3, 5, 5);
+    cout << endl;
+}
---
+++ Сдать до 2022_02_13.cpp
@@ -0,0 +1,104 @@
+﻿
+
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	srand (time(NULL)); 
+
+	int const size = 7;
+	int const size1 = 5;
+	int ar [size] [size1];	
+	float sum = 0;
+	float sr = 0;	
+	int min1 = 90*size1;
+	int max1 = size1-1;
+	int min2 = 90*size;
+	int max2 = size-1;
+	int number_line = 0;
+	int number_column = 0;
+	for (int i = 0, sums; i < size; i++)	
+	{				
+		sums = 0;
+		for (int j = 0; j < size1; j++)
+		{			
+			ar[i][j] = 1 + rand() % (91 + 1);			
+			cout << ar[i][j] << "\t ";	
+			sum += ar[i][j];                             //Сумма всего массива
+			sr = sum/ (size*size1);		                 //Среднеарифмитическое 
+			sums += ar[i][j];                            //Сумма строк	
+			
+		}		
+		for (int j = 0; j < size1; j++)
+		{
+			if (sums > max1)
+			{
+				max1 = sums;
+				number_line = i + 1;                      //Номер строки с максимальной суммой
+			}
+			if (sums < min1) min1 = sums;				
+		}
+		
+		
+		cout << " | " << sums;             
+		cout << endl;
+	}	
+	
+	cout << "__________________________________________ " << endl;	
+	
+	int min = ar[0][0];                                   //Мин. макс. значение массива 
+	int max = ar[0][0];
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size1; j++)		
+		{			
+			if (ar[i][j] < min) min = ar[i][j];
+			if (ar[i][j] > max)	max = ar[i][j];
+		}	
+	}			
+	
+	for (int j = 0, sumj; j < size1; j++)
+	{
+		sumj = 0;                                          //Сумма столбцов  		
+		for (int i = 0; i < size; i++)
+		{
+			sumj += ar[i][j];
+		}
+		for (int i = 0; i < size1; i++)
+		{
+			if (sumj > max2) max2 = sumj;
+			if (sumj < min2)
+			{
+				min2 = sumj;
+				number_column = j + 1;                     //Номер столбца с минимальной суммой
+			}
+		}				
+		cout << " " << sumj << "\t";
+	}
+
+
+		cout << endl << endl;;
+		cout << "Summa: " << sum << " " << endl;
+		cout << endl;
+		cout << "Sr: " << sr << " " << endl;
+		cout << endl;
+		cout << "MIN " << min << " " << "MAX :" << max << " " << endl;
+		cout << endl;
+		cout << "MIN line:\t " << min1 << "\t " << "MAX line:\t " << max1 << " " << endl;
+		cout << endl;
+		cout << "Namber line MAX: " << number_line << endl;
+		cout << endl;
+		cout << "MIN column:\t " << min2 << "\t " << "MAX column:\t " << max2 << " " << endl;
+		cout << endl;
+		cout << "Namber coiumn MIN: " << number_column << endl;
+		cout << endl;
+
+
+	
+
+}
+
+
