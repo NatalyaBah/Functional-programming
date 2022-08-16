c71a2635f3b4256dca759f7c9435e5c88bb69c6b
NatalyaBah <104036796+NatalyaBah@users.noreply.github.com>
2022-08-16 12:13:30 +0300

    Add files via upload

---
+++ ДЗ до 09.04.2022.cpp
@@ -0,0 +1,136 @@
+﻿// ДЗ до 09.04.2022.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+
+struct Fractions
+{
+    int num;
+    int ben;
+    int whole;
+};
+Fractions division(Fractions d1, Fractions d2)
+{
+    Fractions d3;
+    d3.num = d1.num * d2.ben;
+    d3.ben = d1.ben * d2.num;     
+    if (d3.num / d3.ben >= 1)
+    {
+        d3.whole = (d3.num / d3.ben);
+        d3.num = d3.num - d3.whole * d3.ben;  
+    }
+    else
+    {
+        d3.whole = 0;
+    }
+    for (int i = 2; i <= d3.ben; i++)
+    {
+        if (d3.num % i == 0 && d3.ben % i == 0)
+        {
+            d3.num /= i;
+            d3.ben /= i;
+        }
+    }
+    if (d3.num == 0) d3.ben = d3.num;
+    return d3;
+    
+}
+Fractions mult(Fractions d1, Fractions d2)
+{
+    Fractions d4;
+    d4.num = d1.num * d2.num;
+    d4.ben = d1.ben * d2.ben;  
+    if (d4.num / d4.ben >= 1)
+    {
+        d4.whole = (d4.num / d4.ben);
+        d4.num = d4.num - d4.whole * d4.ben;
+    }
+    else
+    {
+        d4.whole = 0;
+    }
+    for (int i = 2; i <= d4.ben; i++)
+    {
+        if (d4.num % i == 0 && d4.ben % i == 0)
+        {
+            d4.num /= i;
+            d4.ben /= i;
+        }        
+    }    
+    if (d4.num == 0) d4.ben = d4.num;
+    return d4;
+}
+Fractions pluse(Fractions d1, Fractions d2)
+{
+    Fractions d5;
+    d5.num = (d1.num * d2.ben) + (d2.num * d1.ben);
+    d5.ben = d1.ben * d2.ben;    
+    if (d5.num / d5.ben >= 1)
+    {
+        d5.whole = d5.num / d5.ben;
+        d5.num = d5.num - d5.whole * d5.ben;   
+    }   
+    else
+    {
+        d5.whole = 0;
+    }
+    if (d5.num == 0) d5.ben = d5.num;
+    for (int i = 2; i <= d5.ben; i++)
+    {
+        if (d5.num % i == 0 && d5.ben % i == 0)
+        {
+            d5.num /= i;
+            d5.ben /= i;
+        }
+    }
+    if (d5.num == 0) d5.ben = d5.num;
+    return d5;
+
+}
+Fractions minuse(Fractions d1, Fractions d2)
+{
+    Fractions d6;
+    d6.num = (d1.num * d2.ben) - (d2.num * d1.ben);
+    d6.ben = d1.ben * d2.ben;    
+    if(d6.num / d6.ben >= 1)
+    {
+        d6.whole = d6.num / d6.ben;
+        d6.num = d6.num - d6.whole * d6.ben;    
+    }     
+    else
+    {
+        d6.whole = 0;
+    }
+    for (int i = 2; i <= d6.ben; i++)
+    {
+        if (d6.num % i == 0 && d6.ben % i == 0)
+        {
+            d6.num /= i;
+            d6.ben /= i;
+        }        
+    }
+    if (d6.num == 0) d6.ben = d6.num;
+    return d6;
+}
+
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    Fractions d1{ 5, 2 };
+    Fractions d2{ 5, 2 };
+    Fractions d3 = division(d1, d2);
+    cout << "Результат деления\t дробей:\t" << d3.whole << " " << d3.num << "/" << d3.ben << endl;
+    Fractions d4 = mult(d1, d2);
+    cout << "Результат умножения\t дробей:\t" << d4.whole << " " << d4.num << "/" << d4.ben << endl;
+    Fractions d5 = pluse(d1, d2);
+    cout << "Результат сложения\t дробей:\t" << d5.whole << " " << d5.num << "/" << d5.ben << endl;
+    Fractions d6 = minuse(d1, d2);
+    cout << "Результат вычитания\t дробей:\t" << d6.whole << " " << d6.num << "/" << d6.ben << endl;
+    
+}
+
---
+++ ДЗ_3_1.cpp
@@ -0,0 +1,104 @@
+﻿// ДЗ_3_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	//******************* task 1 *********************
+
+
+	double t, s;
+	cout << "input s:";
+	cin >> s;
+	cout << "input t:";
+	cin >> t;
+	double v;
+	cout << s << " / " << t << " = " << s / t << endl;
+	cout << "v = " << s / t << endl;
+	cout << " \n ";
+
+
+	//******************* task 2 *********************
+
+
+	int start, stop;
+	int t1;
+	const int price = 2;
+	int h, m, s5;
+	cout << "h :";
+	cin >> h;
+	cout << "m :";
+	cin >> m;
+	cout << "s :";
+	cin >> s5;
+	cout << "start :" << h << ":" << m << ':' << s5 << endl;
+	int h1, m1, s1;
+	cout << "h1 :";
+	cin >> h1;
+	cout << "m1 :";
+	cin >> m1;
+	cout << "s1 :";
+	cin >> s1;
+	cout << "stop :" << h1 << ":" << m1 << ':' << s1 << endl;
+	start = h * 60 + m + s5 / 60;
+	stop = h1 * 60 + m1 + s1 / 60;
+	t = (stop - start);
+	cout << "riding time :" << t1 << endl;
+	cout << "price :" << t1 * price << " gr ";
+	cout << "\n";
+
+
+	//******************* task 3 *********************
+
+
+	float s7, a, b1, b2, b3, r, r1, r2;
+	cout << "distans :";
+	cin >> a;
+	cout << "consumption :";
+	cin >> s7;
+	cout << "92 :";
+	cin >> b1;
+	cout << "95 :";
+	cin >> b2;
+	cout << "DT :";
+	cin >> b3;
+	r = s7 / 100 * a * b1;
+	r1 = s7 / 100 * a * b2;
+	r2 = s7 / 100 * a * b3;
+	cout << char(218) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(191) << endl;
+	cout << char(124) << "\t 92" << "\t" << char(124) << "\t 95" << "\t" << char(124) << "\t DT" << "\t" << char(124) << endl;
+	cout << char(192) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(217) << endl;
+	cout << char(124) << "\t" << r << "\t" << char(124) << "\t" << r1;
+	cout << "\t" << char(124) << "\t" << r2 << "\t" << char(124) << endl;
+	cout << char(192) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196);
+	cout << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(217) << endl;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
